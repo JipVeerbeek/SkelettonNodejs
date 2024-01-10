192c823913dbf027bb55a303f84988291831ff81
Jip Veerbeek <97675309+JipVeerbeek@users.noreply.github.com>
2024-01-10 12:20:29 +0100

    more env

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,16 @@
-import { Sequelize } from 'sequelize'
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
 
-const connection = new Sequelize('skeletondb', 'root', '', {
-    host: 'localhost',
-    dialect: 'mysql',
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
     logging: false,
-  });
-  
-  export default connection;
+  }
+);
+
+export default connection;
