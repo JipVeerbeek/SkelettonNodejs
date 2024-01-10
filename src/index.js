f4ec62fb22ed53b343da168dc21dd3ebf49c5c4b
Jip Veerbeek <97675309+JipVeerbeek@users.noreply.github.com>
2024-01-10 11:48:08 +0100

    new starting method

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ const server = http.createServer(app);
 const port = process.env.PORT;
 
 server.listen(port, () => {
-  console.log("Server is listening on port " + port);
+  console.log("Server is listening on http://localhost:" + port);
 });
